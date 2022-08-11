9cf3f968592a8e7a0ab60460bb7b313d4e69f3b8
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2022-08-11 16:15:06 -0500

    Added two ADO database connection tests Disabled for now until DB ops can be made generic

---
+++ src/UnitTests/AdoDataConnectionTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gemstone.Data.UnitTests
+{
+    // TODO: Determine how to enable tests for any environment
+    //[TestClass]
+    public class AdoDataConnectionTest
+    {
+        [TestMethod]
+        public void TestSQLServerConnection()
+        {
+            string connectionString = @"Data Source=localhost\SQLEXPRESS; Initial Catalog=openMIC; Integrated Security=SSPI; Connect Timeout=5";
+            string dataProviderString = "AssemblyName=Microsoft.Data.SqlClient; ConnectionType=Microsoft.Data.SqlClient.SqlConnection";
+            //$"AssemblyName={{{typeof(Microsoft.Data.SqlClient.SqlConnection).Assembly.FullName}}}; ConnectionType=Microsoft.Data.SqlClient.SqlConnection; AdapterType=Microsoft.Data.SqlClient.SqlDataAdapter";
+
+            System.Console.WriteLine($"Connection String = {connectionString}");
+            System.Console.WriteLine($"Data Provider String = {dataProviderString}");
+
+            AdoDataConnection connection = new AdoDataConnection(connectionString, dataProviderString);
+
+            Assert.IsTrue(Convert.ToInt32(connection.ExecuteScalar("SELECT COUNT(*) FROM Device")) >= 0);
+        }
+
+        [TestMethod]
+        public void TestSQLiteConnection()
+        {
+            string connectionString = @"Data Source=C:\Program Files\openPDC\Database Scripts\SQLite\openPDC.db; Foreign Keys=True";
+            string dataProviderString = "AssemblyName=Microsoft.Data.Sqlite; ConnectionType=Microsoft.Data.Sqlite.SqliteConnection";
+            //$"AssemblyName={{{typeof(Microsoft.Data.SqlClient.SqlConnection).Assembly.FullName}}}; ConnectionType=Microsoft.Data.SqlClient.SqlConnection; AdapterType=Microsoft.Data.SqlClient.SqlDataAdapter";
+
+            System.Console.WriteLine($"Connection String = {connectionString}");
+            System.Console.WriteLine($"Data Provider String = {dataProviderString}");
+
+            AdoDataConnection connection = new AdoDataConnection(connectionString, dataProviderString);
+
+            Assert.IsTrue(Convert.ToInt32(connection.ExecuteScalar("SELECT COUNT(*) FROM Device")) >= 0);
+        }
+    }
+}

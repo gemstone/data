af82870fbce48748f8c2349c15bb937fc774b4ad
prestoncraw <prestoncrawford530@gmail.com>
2026-08-06 15:45:00 -0400

    Add support for newtonsoft arrays

--- src/Gemstone.Data/Model/RecordFilter.cs
+++ src/Gemstone.Data/Model/RecordFilter.cs
@@ -84,6 +84,20 @@ public class RecordFilter<T> : IRecordFilter where T : class, new()
                         field = typedArray;
                         break;
                     }
+                case Newtonsoft.Json.Linq.JArray array:
+                    {
+                        object?[] typedArray = new object[array.Count];
+
+                        for (int i = 0; i < array.Count; i++)
+                        {
+                            object? element = array[i].ToObject<object>();
+                            object? typedElement = ModelProperty is null ? element : Common.TypeConvertFromString(element?.ToString() ?? "", ModelProperty.PropertyType);
+                            typedArray[i] = typedElement;
+                        }
+
+                        field = typedArray;
+                        break;
+                    }
                 default:
                     {
                         if (value is JsonElement el)
